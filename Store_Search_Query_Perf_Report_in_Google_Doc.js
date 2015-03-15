91ede00fbc8aae8eccead5648e7379223e61466d
pascalflamand <pflamand@conversio.ca>
2015-03-15 15:36:02 -0400

    Google Sheet + Mail
    Ajout de l'onglet du rapport de la Google SpreadSheet et du nom de cet
    onglet dans l’objet du courriel

--- Store_Search_Query_Perf_Report_in_Google_Doc.js
+++ Store_Search_Query_Perf_Report_in_Google_Doc.js
@@ -1,15 +1,18 @@
 /***************************************************
+* Original source 
 * Store Search Query Perf Report in Google Doc
 * Version 1.1
 * CHangelog v1.1 - Removed apiVersion, Updated formatting
 * Created By: Russ Savage
 * FreeAdWordsScripts.com
 ****************************************************/
-var DATE_RANGE = 'LAST_7_DAYS';
+
+var DATE_RANGE = 'LAST_14_DAYS';
 var IGNORE_EXACT = true;
-var TO = ["email_1@my_company.com","email_2@my_company.com"];
-var SPREADSHEET_URL = "your spreadsheet url goes here";  
- 
+var TO = ["email1,email2"];
+var SPREADSHEET_URL = "Google Spreadsheet URL";  
+var ACTIVE_SHEET_NAME = "Active Sheet Name"
+
 function main() {
   var columns = ['AccountDescriptiveName',
                  'CampaignName',
@@ -20,26 +23,25 @@ function main() {
                  'Device',
                  'Impressions',
                  'Clicks',
-                 'AverageCpc',
                  'Cost',
-                 'Ctr',
                  'Conversions',
+                 'AverageCpc',
                  'CostPerConversion',
-                 'ConversionRate'
-                 ];
-  var columnsStr = columns.join(',') + " ";
-    
-  var sheet = SpreadsheetApp.openByUrl(SPREADSHEET_URL).getActiveSheet();
+                 'ConversionRate',
+                 'Ctr'];
+  var columnsStr = columns.join(',') + " "; 
+  var sheet = SpreadsheetApp.openByUrl(SPREADSHEET_URL).getSheetByName(ACTIVE_SHEET_NAME);
   sheet.clear();
   sheet.appendRow(columns);
-    
-  var reportIter = AdWordsApp.report(
+  
+  var reportIter = AdWordsApp.report( //https://developers.google.com/adwords/scripts/docs/reference/adwordsapp/adwordsapp_report
     'SELECT ' + columnsStr +
-    'FROM SEARCH_QUERY_PERFORMANCE_REPORT ' +
+    'FROM SEARCH_QUERY_PERFORMANCE_REPORT ' + // to see all available repports https://www.awql.me/adwords-awql-help/ 
     'DURING ' + DATE_RANGE, {
-      includeZeroImpressions: false
+    includeZeroImpressions: false //Whether or not to include entities that had zero impressions in the report. Defaults to true.
     }).rows();
     
+//I don't fully understand this section     
   while(reportIter.hasNext()) {
     var row = reportIter.next();
     if(IGNORE_EXACT && row['MatchType'].indexOf('exact') >= 0) { continue; }
@@ -49,8 +51,8 @@ function main() {
     }
     sheet.appendRow(rowArray); 
   }
-    
+  
   for(var i in TO) {
-    MailApp.sendEmail(TO[i], "Search Query Report Ready", SPREADSHEET_URL);
+    MailApp.sendEmail(TO[i], ACTIVE_SHEET_NAME , SPREADSHEET_URL);
   }
 }
